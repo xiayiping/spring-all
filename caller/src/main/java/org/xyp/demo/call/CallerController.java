227ba7fecb55119f100acadc4f2734cb1204c76e
matt.xia <matt.xia@tcghl.com>
2023-07-19 12:46:36 +0800

    feature/fakeJira-1 fake jira

--- caller/src/main/java/org/xyp/demo/call/CallerController.java
+++ caller/src/main/java/org/xyp/demo/call/CallerController.java
@@ -1,10 +1,12 @@
 package org.xyp.demo.call;
 
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Slf4j
 @RestController
@@ -12,15 +14,24 @@ import org.springframework.web.client.RestTemplate;
 public class CallerController {
 
     private final RestTemplate restTemplate;
+    private final WebClient webClient;
 
-    public CallerController(RestTemplate template) {
+    public CallerController(RestTemplate template, WebClient webClient) {
         this.restTemplate = template;
+        this.webClient = webClient;
     }
 
     @GetMapping("/call")
     public String call() {
         log.info("call from caller");
-        var echo = restTemplate.getForObject("http://localhost:8082/echo/echo", String.class);
+        val echo = restTemplate.getForObject("https://localhost:8092/echo/echo", String.class);
+        return "call " + echo;
+    }
+
+    @GetMapping("/callAsync")
+    public String callAsync() {
+        log.info("call from caller");
+        val echo = webClient.get().uri("/echo").retrieve().bodyToMono(String.class).block();
         return "call " + echo;
     }
 }
--- echo/src/main/java/org/xyp/demo/echo/EchoController.java
+++ echo/src/main/java/org/xyp/demo/echo/EchoController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @Slf4j
 @RestController
 @RequestMapping("/echo")
@@ -13,6 +15,7 @@ public class EchoController {
     @GetMapping("/echo")
     public String call() {
         log.info("echo from echo");
-        return "echo";
+        return "echo " + new Random().nextInt(100);
     }
+
 }
