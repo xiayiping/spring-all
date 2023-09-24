53a791bc602e26c1950d3d0a6bf5abb7f01c0201
matt.xia <matt.xia@tcghl.com>
2023-09-24 11:43:00 +0800

    feat: the log link still not work

--- config/script.sh
+++ config/script.sh
@@ -1,4 +1,4 @@
-
+JAVA_TOOL_OPTIONS="-javaagent:opentelemetry-javaagent.jar"; OTEL_TRACES_EXPORTER=otlp; OTEL_METRICS_EXPORTER=otlp; OTEL_LOGS_EXPORTER=; OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:5555;
 
 export JAVA_TOOL_OPTIONS="-javaagent:/d/develop/spring-all/opentelemetry-javaagent.jar"
 export OTEL_TRACES_EXPORTER=otlp
--- demo-caller/src/main/java/org/xyp/demo/call/CallerMainApp.java
+++ demo-caller/src/main/java/org/xyp/demo/call/CallerMainApp.java
@@ -1,22 +1,23 @@
 package org.xyp.demo.call;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Metrics;
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientSsl;
 import org.springframework.boot.ssl.SslBundles;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-//import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
-//import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.xyp.demo.api.EchoService;
 
 import java.util.Optional;
 
+@Slf4j
 @SpringBootApplication
 //@LoadBalancerClients({
 //    @LoadBalancerClient("echoServer")
@@ -77,4 +78,15 @@ public class CallerMainApp {
 //            }
 //        };
 //    }
+
+    @Bean
+    @ConditionalOnClass(name = "io.opentelemetry.javaagent.OpenTelemetryAgent")
+    public MeterRegistry otelRegistry() {
+        log.warn("register open tel metrics");
+        Optional<MeterRegistry> otelRegistry = Metrics.globalRegistry.getRegistries().stream()
+                .filter(r -> r.getClass().getName().contains("OpenTelemetryMeterRegistry"))
+                .findAny();
+        otelRegistry.ifPresent(Metrics.globalRegistry::remove);
+        return otelRegistry.orElse(null);
+    }
 }
