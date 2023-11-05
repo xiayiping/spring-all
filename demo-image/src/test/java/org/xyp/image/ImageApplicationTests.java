56a4229b87dabe5e0e33334566dc75c7d196dd9b
xiayiping <summer_xyp@qq.com>
2023-11-05 13:02:28 +0800

    chore: remove some useless test

--- demo-image/src/test/java/org/xyp/image/ImageApplicationTests.java
+++ demo-image/src/test/java/org/xyp/image/ImageApplicationTests.java
@@ -1,9 +1,7 @@
 package org.xyp.image;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ImageApplicationTests {
 
     @Test
