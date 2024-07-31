279df71d89ea6780752eb37b64efc17252a09e0d
xiayiping <summer_xyp@qq.com>
2024-07-31 23:05:14 +0800

    feat: test session timeout

--- document/note.home.md
+++ document/note.home.md
@@ -154,6 +154,31 @@ While PKCE and SCRAM-SHA-256 both enhance security in different authentication s
 
 
 org.springframework.boot.autoconfigure.web.reactive.WebSessionIdResolverAutoConfiguration
-org.springframework.boot.autoconfigure.web.reactive.WebSessionIdResolverAutoConfiguration
+
+
+## Functional Programme
+
+### Monoid
+
+- closure (set of values + combination operator)  : useful for collection folder
+- Associativity   : useful for parallel
+- Identity Element    : first element in collection folder | Empty element
+
+The combination of Monoid is also a Monoid.
+
+### Monad
+
+#### Effect
+List<_>  Option<_>  Resunt<>  Task<>  Async<>  etc......
+
+### Design pattern
+
+- map: map a non-monoid to a Monoid
+- map: map inside Effect. Sample:
+```javascript
+    let add42ToEach = List.map (+42)
+    add42ToEach [1,2,3]
+```
+
 
 </div>
--- spring-sample/oauth2-client/src/main/java/org/xyp/sample/spring/oauth2client/Oauth2ClientWebConfig.java
+++ spring-sample/oauth2-client/src/main/java/org/xyp/sample/spring/oauth2client/Oauth2ClientWebConfig.java
@@ -1,6 +1,7 @@
 package org.xyp.sample.spring.oauth2client;
 
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.autoconfigure.security.reactive.PathRequest;
 import org.springframework.boot.autoconfigure.security.reactive.StaticResourceRequest;
@@ -9,13 +10,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
 import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher;
+
+import java.net.URI;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 import static org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher.MatchResult;
@@ -64,16 +69,46 @@ public class Oauth2ClientWebConfig {
                 .anyExchange().authenticated()
             )
         ;
-        
+
         if (use403ThanLoginPage) {
             http.exceptionHandling(cus -> cus.authenticationEntryPoint(new HttpStatusServerEntryPoint(HttpStatus.FORBIDDEN)));
         }
-        http.oauth2Login(login-> login.authenticationMatcher(new PathPatternParserServerWebExchangeMatcher("/oauth2/login/code/{registrationId}")));
+        http.oauth2Login(login -> login.authenticationMatcher(new PathPatternParserServerWebExchangeMatcher("/oauth2/login/code/{registrationId}")));
         http.oauth2Client(withDefaults());
+
+//        http.addFilterAfter((exchange, chain) -> {
+//            System.out.println("ssss");
+//            return exchange.getSession()
+//                .map(s -> {
+//                    System.out.println(s.getAttributes());
+//                    return s;
+//                })
+//                .flatMap(s -> {
+//
+//                    if (!s.isExpired()) {
+//                        System.out.println("valid");
+//                        System.out.println("valid");
+//                        System.out.println("valid");
+//                        System.out.println("valid");
+//                        return chain.filter(exchange);
+//                    } else {
+//                        System.out.println("expired ");
+//                        System.out.println("expired ");
+//                        System.out.println("expired ");
+//                        System.out.println("expired ");
+//
+//                        val resp = exchange.getResponse();
+//                        resp.setStatusCode(HttpStatus.FOUND);
+//                        resp.getHeaders().setLocation(URI.create("/ttt/oauth2/authorization/{registrationId}"));
+//                        return resp.setComplete();
+//                    }
+//                });
+//        }, SecurityWebFiltersOrder.FORM_LOGIN);
 
         http.logout((logout) -> logout
             .logoutSuccessHandler(oidcLogoutSuccessHandler(clientRegistrationRepository))
         );
+
         return http.build();
     }
 
@@ -85,7 +120,8 @@ public class Oauth2ClientWebConfig {
 
         // Sets the location that the End-User's User Agent will be redirected to
         // after the logout has been performed at the Provider
-        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}");
+//        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}");
+        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}/oauth2/authorization/{registrationId}");
 
         return oidcLogoutSuccessHandler;
     }
